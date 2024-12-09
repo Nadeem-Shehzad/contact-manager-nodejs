819a4818c6f91deb94a1d6dc150c24eb86efb2fb
Nadeem-Shehzad <nadeem.dev@outlook.com>
2024-12-09 14:56:03 +0500

    modify for routing and controllers

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const customErrorHandler = require('./middlewares/errorHandler');
 require('dotenv').config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/',(req,res) => {
-    res.status(200).json({message : `Welcome in Contact Manager Api`});
-});
+app.use(express.json());
+app.use('/api/contacts',require('./routes/contactRoutes'));
+app.use(customErrorHandler)
 
 app.listen(PORT,() => {
     console.log(`Server is running on PORT : ${PORT}`);
