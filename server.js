bb7a20de6ba11c3ad3f5edb34832c0cb7b30d499
Nadeem-Shehzad <nadeem.dev@outlook.com>
2024-12-09 23:31:49 +0500

    add user functionality

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const customErrorHandler = require('./middlewares/errorHandler');
+const contactRoutes = require('./routes/contactRoutes');
+const userRoutes = require('./routes/userRoutes');
 const connectDB = require('./config/dbConnection');
 require('dotenv').config();
 connectDB();
@@ -9,9 +11,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/api/contacts',require('./routes/contactRoutes'));
+app.use('/api/contacts', contactRoutes);
+app.use('/api/user', userRoutes);
 app.use(customErrorHandler)
 
-app.listen(PORT,() => {
+app.listen(PORT, () => {
     console.log(`Server is running on PORT : ${PORT}`);
 });
